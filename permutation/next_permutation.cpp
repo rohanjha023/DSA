27e6904a891732f2f4e8921b592dfdc4464fe727
rohanjha023 <rohanjha023@gmail.com>
2026-04-16 11:06:04 +0530

    basic

---
+++ permutation/next_permutation.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+void nextPermutation(vector<int>& nums) {
+    int n = nums.size();
+    int index = -1;
+
+    for (int i = n - 2; i >= 0; i--) {
+        if (nums[i] < nums[i + 1]) {
+            index = i;
+            break;
+        }
+    }
+
+    if (index == -1) {
+        reverse(nums.begin(), nums.end());
+        return;
+    }
+
+    for (int i = n - 1; i > index; i--) {
+        if (nums[i] > nums[index]) {
+            swap(nums[i], nums[index]);
+            break;
+        }
+    }
+
+    reverse(nums.begin() + index + 1, nums.end());
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> nums(n);
+    for (int i = 0; i < n; i++) {
+        cin >> nums[i];
+    }
+
+    nextPermutation(nums);
+
+    for (int x : nums) {
+        cout << x << " ";
+    }
+
+    return 0;
+}
